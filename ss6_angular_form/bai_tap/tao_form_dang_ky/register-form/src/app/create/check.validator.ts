cb71de02bfcc7cb858fd137be60a50c5c60195ac
mao77 <quocanh.89dn@gmail.com>
2022-07-07 08:05:19 +0700

    update ss6 7/7/22

--- ss6_angular_form/bai_tap/tao_form_dang_ky/register-form/src/app/create/check.validator.ts
+++
@@ -1,8 +0,0 @@
-import {AbstractControl, ValidationErrors} from '@angular/forms';
-
-export function check(control1: AbstractControl,control2:AbstractControl): ValidationErrors | null {
-  if (control1.value == control2.value) {
-    return {check:true}
-  }
-  return null
-}
--- ss6_angular_form/bai_tap/tao_form_dang_ky/register-form/src/app/create/create.component.ts
+++ ss6_angular_form/bai_tap/tao_form_dang_ky/register-form/src/app/create/create.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
-import {check} from "./check.validator";
+
 
 @Component({
   selector: 'app-create',
@@ -8,28 +8,36 @@ import {check} from "./check.validator";
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-  createForm: FormGroup;
+
+  createForm = new FormGroup({
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl('',[Validators.required]),
+      confirmPassword: new FormControl('',[Validators.required]),
+      country: new FormControl('',[Validators.required]),
+      age: new FormControl('',[Validators.min(19)]),
+      gender: new FormControl('',[Validators.required]),
+      phone: new FormControl('',[Validators.pattern(' /^\\+84\\d{9,10}$/')])
+    }, this.check
+  );
   countries: string[] = ['USA', 'VN', 'ENG', 'RUSS']
 
-  constructor() {
-    this.createForm = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(''),
-      confirmPassword: new FormControl(''),
-      country: new FormControl(''),
-      age: new FormControl(''),
-      gender: new FormControl(''),
-      phone: new FormControl('')
-    });
-
+  check(check: AbstractControl): ValidationErrors | null {
+    if (check.value.password != check.value.confirmPassword) {
+      return {"check": true}
+    }
+    return null;
   }
 
-get password() {
+  constructor() {
+  }
+
+  get password() {
     return this.createForm.get('password')
-}
-get confirmPassword() {
+  }
+
+  get confirmPassword() {
     return this.createForm.get('confirmPassword')
-}
+  }
 
   ngOnInit(): void {
   }
